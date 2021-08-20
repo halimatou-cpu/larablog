ae562e1877de7ac592ab50fd5ffc992359023a11
halimatou-cpu <halimatoubah.bh@gmail.com>
2021-08-20 22:31:05 +0200

    dynamic search system according to words in the title or to category

--- app/Http/Livewire/Filters.php
+++ app/Http/Livewire/Filters.php
@@ -19,13 +19,18 @@ class Filters extends Component
         });
 
         return view('livewire.filters', [
-            // 'articles' => (empty($this->activeFilters))
-            //     ? Article::all()
-            //     : Article::whereIn('category_id', array_keys($this->activeFilters))->get(),
-            'articles' => (empty($this->search))
-                ? Article::all()
-                // : Article::where('title', '%'.$this->search.'%')->get(),
-                : Article::where('title', "like", "%$this->search%")->get(),
+            'articles' => (empty($this->activeFilters))
+                ? ((empty($this->search)) 
+                    ? Article::all()
+                    : Article::where('title', "like", "%$this->search%")->get())
+                : Article::whereIn('category_id', array_keys($this->activeFilters))->get(),
         ]);
     }
 }
+
+// 'articles' => (empty($this->activeFilters))
+//                 ? Article::all()
+//                 : Article::whereIn('category_id', array_keys($this->activeFilters))->get(),
+//             'articles' => (empty($this->search))
+//                 ? Article::all()
+//                 : Article::where('title', "like", "%$this->search%")->get(),
